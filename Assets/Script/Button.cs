afe29d7e6ecf91a075c4b453352d7c676b683074
ZericZhu <zeric1119@yahoo.com>
2022-02-02 12:10:34 -0500

    uiui

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Debug.Log("wwww");
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }else if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+    }
+}
---
+++ Assets/Script/ClassDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ClassDemo : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text This_Text;
+
+    void Start()
+    {
+        This_Text = this.GetComponent<TMP_Text>();
+        This_Text.text = "22222";
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
